86b3f314f74d2befc87d1be697cf55742e576172
Zach Urbach <urbach-zach@gmail.com>
2014-06-24 22:28:14 -0600

    added the calls
    put the Disemvoweler calls in

--- Disemvoweler/Program.cs
+++ Disemvoweler/Program.cs
@@ -10,11 +10,20 @@ namespace Disemvoweler
     {
         static void Main(string[] args)
         {
+            //this program will be awesome
+            
+            Console.WriteLine("The Disemvoweler");
+            Disemvoweler("Nickleback is my favorite band. Their songwriting can't be beat!");
+            Console.WriteLine();
+            Disemvoweler("How many bears could bear grylls grill if bear grylls could grill bears?");
+            Console.WriteLine();
+            Disemvoweler("I'm a code ninja, baby. I make the functions and I make the calls.");
+            Console.WriteLine();
 
-            //this program will be awsome
-
+            //keeps console open
             Console.ReadKey();
-
         }
+       
+
     }
 }
